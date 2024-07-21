30fccd2aa0879d832af6844b859fed51bf1efcca
kumarpintubkp1997@gmail.com <you@example.com>
2024-07-21 12:56:26 +0530

    Add new file user profile

---
+++ app/Models/UserProfileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfileController extends Model
+{
+    use HasFactory;
+}
